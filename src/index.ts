7e3a377f8e984d39606d0a4ac95305bfd9efa252
Anton Ödman <anton.odman@gmail.com>
2021-05-26 22:48:39 +0200

    Updated launch files

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,3 @@ const statement = process.argv[2];
 if (statement) console.log(new RegCode().convert(statement));
 
 export { RegCode };
-
-const regexConvert = new RegCode();
-const regexCode = "normal(tjena)normal(3) normal(2)";
-// const regexCode = "normal(hej)";
-console.log(regexCode);
-const result = regexConvert.convert(regexCode);
-console.log(result);
-
-// const hasMatch = regexConvert.hasMatch(regexCode, "33r3");
-// console.log(hasMatch);
-
-// const match = regexConvert.match(regexCode, "33r3");
-// console.log(match);
